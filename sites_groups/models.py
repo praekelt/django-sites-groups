8e8a5dac7f6740cebc0cc21ec176d92816a8a040
hedleyroos <hedleyroos@gmail.com>
2012-04-30 22:18:44 +0200

    Provide selection widget

--- sites_groups/models.py
+++ sites_groups/models.py
@@ -10,6 +10,13 @@ class SitesGroup(models.Model):
         'sites.Site',
         help_text='Sites that belong to this group.',
     )
-   
+
+    class Meta:
+        ordering = ('title',)
+
     def __unicode__(self):
         return self.title
+
+    @property
+    def site_ids(self):        
+        return self.sites.all().values_list('id', flat=True)
