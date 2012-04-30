f2c7e917cef4d4a525a24e975b3841acc96ee922
hedleyroos <hedleyroos@gmail.com>
2012-04-30 21:16:53 +0200

    Skeleton

---
+++ sites_groups/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class SiteGroup(models.Model):
+    title = models.CharField(
+        max_length=256,
+        help_text='A short descriptive title.',
+    )
+    sites = models.ManyToManyField(
+        'sites.Site',
+        help_text='Sites that belong to this group.',
+    )
+   
+    def __unicode__(self):
+        return self.title
